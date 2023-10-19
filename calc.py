95b6fe85e24124dec18db820bd72b7db0333dada
Afreen <131018705+AfreenInnovates@users.noreply.github.com>
2023-10-19 21:31:35 +0530

    Create calc.py

---
+++ calc.py
@@ -0,0 +1,48 @@
+# Function to add two numbers
+def add(x, y):
+    return x + y
+
+# Function to subtract two numbers
+def subtract(x, y):
+    return x - y
+
+# Function to multiply two numbers
+def multiply(x, y):
+    return x * y
+
+# Function to divide two numbers
+def divide(x, y):
+    if y == 0:
+        return "Cannot divide by zero"
+    return x / y
+
+# Display the menu of operations
+print("Select operation:")
+print("1. Addition")
+print("2. Subtraction")
+print("3. Multiplication")
+print("4. Division")
+
+while True:
+    choice = input("Enter choice (1/2/3/4): ")
+
+    if choice not in ('1', '2', '3', '4'):
+        print("Enter valid choice.")
+    else:
+        num1 = float(input("Enter first number: "))
+        num2 = float(input("Enter second number: "))
+
+        # Perform the selected operation
+        if choice == '1':
+            result = add(num1, num2)
+        elif choice == '2':
+            result = subtract(num1, num2)
+        elif choice == '3':
+            result = multiply(num1, num2)
+        elif choice == '4':
+            result = divide(num1, num2)
+
+        # Display the result
+        print("Result:", result)
+
+        break
